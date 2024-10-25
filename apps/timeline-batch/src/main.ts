2456321d84ef003042cffa4ec18605b2668b756b
ryanrepos <mamarasulov911@gmail.com>
2024-10-25 19:31:57 +0400

    feat: develop env

--- apps/timeline-batch/src/main.ts
+++ apps/timeline-batch/src/main.ts
@@ -3,6 +3,6 @@ import { TimelineBatchModule } from './timeline-batch.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(TimelineBatchModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.PORT_BATCH ?? 3000);
 }
 bootstrap();
--- apps/timeline-batch/src/timeline-batch.module.ts
+++ apps/timeline-batch/src/timeline-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TimelineBatchController } from './timeline-batch.controller';
 import { TimelineBatchService } from './timeline-batch.service';
+import { ConfigModule } from '@nestjs/config'; 
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()], // for .env usage],
   controllers: [TimelineBatchController],
   providers: [TimelineBatchService],
 })
